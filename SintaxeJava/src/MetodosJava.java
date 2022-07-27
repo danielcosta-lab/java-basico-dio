a3b3bc60a69cff7d1ba25380c9de65ce89784e3f
Daniel <daniel.costa0494@gmail.com>
2022-07-27 12:00:47 -0300

    -

---
+++ SintaxeJava/src/MetodosJava.java
@@ -0,0 +1,5 @@
+//metodos: funções ou sub-rotinas disponiveis dentro de uma classe
+
+// creterios de nomeação de metodos: (não é obrigatorio mas recomendavel)
+// * deve ser nomeado como verbo
+// * seguir o padrão camelCase (todas as letras minusculas, com exceção da primeira letra da segunda palavra)
