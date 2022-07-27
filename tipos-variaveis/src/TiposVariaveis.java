18a762f2872457bc7ed33af887a2e79fb32d7b5a
Daniel <daniel.costa0494@gmail.com>
2022-07-27 12:00:37 -0300

    -

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // tipos de variaveis primitivas
+        byte idade = 123;
+        short ano = 2022;
+        int cep = 07031000;
+        long cpf = 12345678912L;
+        float pi = 3.14F;
+        double salario = 1275.03;
+
+        final double VALORTOTAL = 235; //constante, não pode ser alterado ao usar o FINAL seguido do tipo.
+
+        VALORTOTAL = 255;
     }
 }
